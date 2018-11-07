a880166b6f15291fe5e023f68179814825309550
Gerben van Dooren <pedooren@avans.nl>
2018-11-07 14:40:15 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- Blindwalls/app/src/main/java/nl/ralphrouwen/blindwalls/DetailedActivity.java
+++ Blindwalls/app/src/main/java/nl/ralphrouwen/blindwalls/DetailedActivity.java
@@ -1,13 +1,40 @@
 package nl.ralphrouwen.blindwalls;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailedActivity extends AppCompatActivity {
+
+    TextView muralAuthorName;
+    TextView muralDescription;
+    TextView muralYear;
+    ImageView muralImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detailed);
+
+        muralAuthorName = findViewById(R.id.detailedActivity_muralName);
+        muralDescription = findViewById(R.id.detailedActivity_muralDescription);
+        muralYear = findViewById(R.id.detailedActivity_muralYear);
+        muralImage = findViewById(R.id.detailedActivity_muralImage);
+
+        Intent intent = getIntent();
+        Mural mural = (Mural) intent.getSerializableExtra("MURAL_OBJECT");
+        //System.out.println(mural.);
+
+        muralAuthorName.setText(mural.getAuthor());
+        muralDescription.setText(mural.getDescriptionENG());
+        muralYear.setText(mural.getYear());
+
+        int resId = this.getResources().getIdentifier(
+                mural.getImageURL(),
+                "drawable",
+                this.getPackageName());
+        muralImage.setImageResource(resId);
     }
 }
