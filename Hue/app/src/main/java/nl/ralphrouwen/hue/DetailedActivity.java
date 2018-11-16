c07dab253cf4b09b1251ea2ff38cc66bc95694b8
Gerben van Dooren <pedooren@avans.nl>
2018-11-16 09:42:49 +0100

    [Added] DetailedActivity

---
+++ Hue/app/src/main/java/nl/ralphrouwen/hue/DetailedActivity.java
@@ -0,0 +1,18 @@
+package nl.ralphrouwen.hue;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed);
+
+        Intent intent = getIntent();
+
+
+    }
+}
