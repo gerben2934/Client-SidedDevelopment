3b8d35f6c8362000a4aa971ec52106775299c7c1
Ralph <rasrouwe@avans.nl>
2018-11-20 12:55:19 +0100

    gui fix

--- Hue/app/src/main/java/nl/ralphrouwen/hue/Activitys/MainActivity.java
+++ Hue/app/src/main/java/nl/ralphrouwen/hue/Activitys/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bridges.add(new Bridge(1,"Emulator Ralph Thuis", "http://192.168.178.29", ""));
-        bridges.add(new Bridge(2,"Emulator Ralph school", "http://145.49.45.24", "97432bff21e2641012f54eb62615bea"));
+        bridges.add(new Bridge(2,"Emulator Ralph school", "http://145.49.45.24", "78481ef239c45c87b9b42bba3b25101"));
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         mRecyclerView.setHasFixedSize(true);
 
