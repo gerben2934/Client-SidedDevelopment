a8ba0c5af8b4bda6bff1c79df1bb149f7426bc1e
Ralph <ralph@rouwen.com>
2018-12-05 16:07:55 +0100

    button toegevoegd

--- LocationAwareApp/app/src/main/java/nl/ralphrouwen/locationawareapp/Activitys/MainActivity.java
+++ LocationAwareApp/app/src/main/java/nl/ralphrouwen/locationawareapp/Activitys/MainActivity.java
@@ -10,6 +10,10 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -21,16 +25,34 @@ import nl.ralphrouwen.locationawareapp.R;
 public class MainActivity extends AppCompatActivity implements MapFragment.OnFragmentInteractionListener {
 
     public static final String PARKED_URL = "parkedURL";
-
+    ImageButton parkbutton;
+    boolean parkButtonPressed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        parkButtonPressed = false;
+
+        parkbutton = findViewById(R.id.parkbutton);
+
 
 /*        FragmentActivity MapFragment = new FragmentActivity();
         MapFragment.findViewById(R.id.)*/
 
+    }
+
+    public void parkButtonPressed(View view)
+    {
+        if(!parkButtonPressed)
+        {
+            parkbutton.setImageResource(R.drawable.parkbutton3);
+            parkButtonPressed = true;
+        }else{
+            parkbutton.setImageResource(R.drawable.parkbutton5);
+            parkButtonPressed = false;
+        }
+        Log.i("HALLO", "puttonpressed");
     }
 
     @Override
